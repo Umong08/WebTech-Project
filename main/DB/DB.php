135b34462eb033bbf29e0516681fc173ae7d8940
sakib <h22460721@gmail.com>
2026-01-20 23:16:20 +0600

    DB

--- main/DB/DB.php
+++ main/DB/DB.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";  
-$db = "university management system";
+$db = "university  system";
 
 $conn = mysqli_connect($host, $user, $password, $db);
 
